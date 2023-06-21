470b3bebbc60330dce8be1cadce5a944296f754b
_woo_s.j <sjwoo1999@korea.ac.kr>
2023-06-21 15:19:47 +0900

    ui setting done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,199 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./style.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <html>
+      <head>
+        <title>Test</title>
+        <style></style>
+      </head>
+
+      <body>
+        <div
+          class="container"
+          style={{
+            border: "3px solid black",
+            padding: "10px",
+            margin: "20px",
+            width: "300px",
+            height: "750px",
+            float: "left",
+            width: "20%",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <select
+            name="node_id"
+            style={{
+              width: "200px",
+              height: "50px",
+            }}
+            class="custom-select"
+          >
+            <option value="node_id" selected>
+              node_id
+            </option>
+          </select>
+
+          <select
+            name="node_guid"
+            style={{
+              width: "200px",
+              height: "50px",
+            }}
+            class="custom-select2"
+          >
+            <option value="node_guid" selected>
+              node_guid
+            </option>
+          </select>
+
+          <select
+            name="node_category_name"
+            style={{
+              width: "200px",
+              height: "50px",
+            }}
+            class="custom-select3"
+          >
+            <option value="node_category_name" selected>
+              node_category_name
+            </option>
+          </select>
+
+          <select
+            name="node_family_name"
+            style={{
+              width: "200px",
+              height: "50px",
+            }}
+            class="custom-select4"
+          >
+            <option value="node_family_name" selected>
+              node_family_name
+            </option>
+          </select>
+
+          <select
+            name="link_id"
+            style={{
+              width: "200px",
+              height: "50px",
+            }}
+            class="custom-select-link"
+          >
+            <option value="link_id" selected>
+              link_id
+            </option>
+          </select>
+
+          <select
+            name="link_guid"
+            style={{
+              width: "200px",
+              height: "50px",
+            }}
+            class="custom-select-link2"
+          >
+            <option value="link_guid" selected>
+              link_guid
+            </option>
+          </select>
+
+          <select
+            name="start_node_id"
+            style={{
+              width: "200px",
+              height: "50px",
+            }}
+            class="custom-select-link3"
+          >
+            <option value="start_node_id" selected>
+              start_node_id
+            </option>
+          </select>
+
+          <select
+            name="end_node_id"
+            style={{
+              width: "200px",
+              height: "50px",
+            }}
+            class="custom-select-link4"
+          >
+            <option value="end_node_id" selected>
+              end_node_id
+            </option>
+          </select>
+
+          <select
+            name="start_node_guid"
+            style={{
+              width: "200px",
+              height: "50px",
+            }}
+            class="custom-select-link5"
+          >
+            <option value="start_node_guid" selected>
+              start_node_guid
+            </option>
+          </select>
+
+          <select
+            name="end_node_guid"
+            style={{
+              width: "200px",
+              height: "50px",
+            }}
+            class="custom-select-link6"
+          >
+            <option value="end_node_guid" selected>
+              end_node_guid
+            </option>
+          </select>
+
+          <select
+            name="link_category_name"
+            style={{
+              width: "200px",
+              height: "50px",
+            }}
+            class="custom-select-link7"
+          >
+            <option value="link_category_name" selected>
+              link_category_name
+            </option>
+          </select>
+
+          <select
+            name="link_family_name"
+            style={{
+              width: "200px",
+              height: "50px",
+            }}
+            class="custom-select-link8"
+          >
+            <option value="link_family_name" selected>
+              link_family_name
+            </option>
+          </select>
+        </div>
+
+        <div
+          style={{
+            border: "3px solid black",
+            padding: "10px",
+            margin: "20px",
+            width: "300px",
+            height: "750px",
+            float: "left",
+            width: "1000px",
+          }}
+        ></div>
+      </body>
+    </html>
   );
-}
+};
 
 export default App;
